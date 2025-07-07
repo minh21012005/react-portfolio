db868089010c98bd4fc576f8d652f3583cf36c40
minh21012005 <baminhss@gmail.com>
2025-07-07 14:27:35 +0700

    update avatar

--- src/components/sections/introduction/index.tsx
+++ src/components/sections/introduction/index.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from 'react-bootstrap';
 import Tilt from "react-parallax-tilt";
-import avatarImg from "assets/ava.png";
+import avatarImg from "assets/avatar.png";
 import './introduction.scss';
 import { useTranslation } from 'react-i18next';
 
