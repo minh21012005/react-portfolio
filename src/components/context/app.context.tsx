f553137844acbb853baafb6db369a8b9e308c3bd
minh21012005 <baminhss@gmail.com>
2025-07-07 14:43:46 +0700

    update theme

--- src/components/context/app.context.tsx
+++ src/components/context/app.context.tsx
@@ -5,40 +5,36 @@ interface IAppContext {
     setTheme: (value: ThemeContextType) => void;
 }
 
-type ThemeContextType = "light" | 'dark';
+type ThemeContextType = "light" | "dark";
 
 const AppContext = createContext<IAppContext | null>(null);
 
 export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
     const [theme, setTheme] = useState<ThemeContextType>(() => {
-        const initialTheme = localStorage.getItem("theme") as ThemeContextType || "light";
+        // Mặc định là "dark" nếu không có trong localStorage
+        const initialTheme = (localStorage.getItem("theme") as ThemeContextType) || "dark";
         return initialTheme;
     });
 
     useEffect(() => {
-        const mode = localStorage.getItem("theme") as ThemeContextType;
-        if (mode) {
-            setTheme(mode);
-            document.documentElement.setAttribute('data-bs-theme', mode);
-        }
-    }, [])
+        // Luôn set lại attribute theme khi component mount
+        document.documentElement.setAttribute('data-bs-theme', theme);
+        // Lưu theme hiện tại vào localStorage khi theme thay đổi
+        localStorage.setItem("theme", theme);
+    }, [theme]); // lắng nghe theme thay đổi
 
     return (
-        <AppContext.Provider value={{
-            theme, setTheme
-        }}>
+        <AppContext.Provider value={{ theme, setTheme }}>
             {children}
         </AppContext.Provider>
     );
-}
+};
 
 export const useCurrentApp = () => {
     const currentAppContext = useContext(AppContext);
 
     if (!currentAppContext) {
-        throw new Error(
-            "useCurrentApp has to be used within <AppContext.Provider>"
-        );
+        throw new Error("useCurrentApp has to be used within <AppContext.Provider>");
     }
 
     return currentAppContext;
